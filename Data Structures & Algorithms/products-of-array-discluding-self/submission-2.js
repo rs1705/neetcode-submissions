dd8dd008a05850a0f19ef482c55789910176ece3
Rahul Saini <asmrahul17@gmail.com>
2026-05-20 18:10:15 +0530

    Add: products-of-array-discluding-self - submission-2

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-2.js
@@ -0,0 +1,24 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[]}
+     */
+   productExceptSelf = function (nums) {
+  let result = [];
+  let prefix = new Array(nums.length).fill(1);
+  let suffix = new Array(nums.length).fill(1);
+  for (let i = 1; i < nums.length; i++) {
+    prefix[i] = prefix[i - 1] * nums[i - 1];
+  }
+
+  for (let i = nums.length - 2; i >= 0; i--) {
+    suffix[i] = suffix[i + 1] * nums[i + 1];
+  }
+
+  for (let i = 0; i < nums.length; i++) {
+    result[i] = prefix[i] * suffix[i];
+  }
+  console.log(result);
+  return result;
+}
+}
